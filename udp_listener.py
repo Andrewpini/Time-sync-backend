0fb58e1bd20d2c34b7034c65313707fd29d39fae
jtguggedal <jantore.guggedal@gmail.com>
2018-02-19 21:33:28 +0100

    Add mandatory server IP argument for UDP listener

--- udp_listener.py
+++ udp_listener.py
@@ -1,31 +1,82 @@
+from util import Interval
+import sys, getopt
 import socket
 import json
-
 import pymysql
 
-# Open database connection
-db = pymysql.connect("localhost","root","admin","positioning", cursorclass = pymysql.cursors.DictCursor)
+DB_ENABLED = True
 
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
+LISTEN_IP = "0.0.0.0"
+LISTEN_PORT = 15000
 
-UDP_IP = "0.0.0.0"
-UDP_PORT = 15000
+BROADCAST_IP = "255.255.255.255"
+BROADCAST_PORT = 10000
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+listenSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+LISTEN_PORT = 15000
+listenSocket.bind((LISTEN_IP, LISTEN_PORT))
 counter = 1
-times = {"192.168.14.82" : 0, "192.168.14.86" : 0, "192.168.14.87" : 0}
+times = {}
 
-while True:
-	rawData, addr = sock.recvfrom(1024)
-	data = json.loads(rawData)
+broadcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-	delta = data['timestamp'] - times[addr[0]]
-	times[addr[0]] = data['timestamp']
-	print(counter , "\tFrom", addr[0], "\tTimestamp: ", times[addr[0]], "\tAddr.: ", data['address'], "\tChannel: ", data['channel'], "\tRSSI: ", data['RSSI'], "\tCRC: ", data['CRC'], "\tLPE: ", data['LPE']) 
-	counter += 1
-	sql = "INSERT INTO rssi_data VALUES(null, '%s', '%d', '%s', %d, %d, %d, %d)" % (addr[0], data['timestamp'] , data['address'], data['channel'], data['RSSI'], data['CRC'], data['LPE'])
+def connectToDatabase:
+    if DB_ENABLED:
+        # Open database connection
+        db = pymysql.connect(host = "localhost", user = "root", passwd = "admin", db = "positioning", port = 3306, cursorclass = pymysql.cursors.DictCursor)
 
-	number_of_rows = cursor.execute(sql)
-	db.commit()
+        # prepare a cursor object using cursor() method
+        cursor = db.cursor()
+        print("Connected to database")
+
+
+def sendServerInfo(ip):
+    message = "position_server: " + ip
+    print(message)
+    broadcastSocket.sendto(message.encode(), (BROADCAST_IP, BROADCAST_PORT))
+
+def main(argv):
+    opts, args = getopt.getopt(argv,"hi:o",["ip=","addr=", "address="])
+    if len(opts) == 0:
+        print("udp_listener.py -i <server IP address>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("udp_listener.py -i <server IP address>")
+            sys.exit()
+        elif opt in ("-i", "-a", "--ip", "--address"):
+            ip = arg
+            print(ip)
+        else:
+            print("udp_listener.py -i <server IP address>")
+            sys.exit(2)
+
+    connectToDatabase()
+
+    interval = Interval.Interval(2, sendServerInfo, args=[ip,])
+    print("Starting Interval, press CTRL+C to stop.")
+    interval.start() 
+
+    while True:
+        try:
+            rawData, addr = listenSocket.recvfrom(1024)
+            data = json.loads(rawData)
+
+            #delta = data['timestamp'] - times[addr[0]]
+            times[addr[0]] = data['timestamp']
+            print(counter , "\tFrom", addr[0], "\tTimestamp: ", times[addr[0]], "\tAddr.: ", data['address'], "\tChannel: ", data['channel'], "\tRSSI: ", data['RSSI'], "\tCRC: ", data['CRC'], "\tLPE: ", data['LPE']) 
+            counter += 1
+
+            if DB_ENABLED:
+                sql = "INSERT INTO rssi_data VALUES(NULL, '%s', '%d', '%s', %d, %d, %d, %d, NULL)" % (addr[0], data['timestamp'] , data['address'], data['channel'], data['RSSI'], data['CRC'], data['LPE'])
+
+                number_of_rows = cursor.execute(sql)
+                db.commit()
+        except KeyboardInterrupt:
+            print("Shutting down interval...")
+            interval.stop()
+            break
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
