be68c5320d4372e3608413eda5a78e584e0e1a43
Anders Storrø <anders_storro@hotmail.com>
2019-06-29 02:40:46 +0200

    Have added aditional functionality for each refined sample entry. Now the dataset contains the local time of when the sample was taken, and the highest diff between the timestamps of each device in that sample. Has also been able to find a really promising example on how to do real time graphs, so that we may drastically improve this functionality for the test rig. Along with the graph we also get the framework for GUI for free (uses QT).

--- processing/learn_pandas.py
+++ processing/learn_pandas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+TIMER_MAX_VAL = 1000000
+
 df = pd.read_csv('raw_sync_data_27-06-2019(14_29).csv')
 
 # print("\nPrint n rows from head and dowwards:")
@@ -25,9 +27,10 @@ df = pd.read_csv('raw_sync_data_27-06-2019(14_29).csv')
 # print(df.Node.unique())
 
 
-df_init = {'Sample #':[], 'RT Clock':[], 'Max drift offset':[]}
+df_init = {'Sample #':[], 'RT Clock':[], 'Max timestamp delta in microseconds':[]}
 df_nice = pd.DataFrame(df_init)
-print(df_nice)
+
+# print(df_nice)
 
 # Get all unique node names and add a new column for each in the data frame
 nodes = df.Node.unique()
@@ -39,13 +42,38 @@ for node_id in nodes:
 sample_ids = df.Event_ID.unique()
 sample_ids.sort()
 for i in sample_ids:
+
     y = {'Sample #': i}
     x = df.loc[df['Event_ID'] == i]
+    y.update({'RT Clock': x.iloc[0]['Local_time']})
+
+    timestamp_list = []
+    timestamp_diff = []
+
     for j, row in x.iterrows():
         y.update({row.Node: row.Timestamp})
-    df_nice = df_nice.append(y, ignore_index=True)
-print(df_nice)
+        timestamp_list.append(row.Timestamp)
 
+    for j in timestamp_list:
+        for k in timestamp_list:
+            diff = abs(j - k)
+            if diff > TIMER_MAX_VAL/2:
+                diff = TIMER_MAX_VAL - diff
+            timestamp_diff.append(diff)
+
+    y.update({'Max timestamp delta in microseconds': max(timestamp_diff)})
+
+
+
+
+    print(timestamp_list)
+    df_nice = df_nice.append(y, ignore_index=True)
+
+df_nice['Sample #'] = df_nice['Sample #'].astype(int)
+df_nice['Max timestamp delta in microseconds'] = df_nice['Max timestamp delta in microseconds'].astype(int)
+
+print(df_nice)
+df_nice.to_csv('refined_results.csv')
 # for i in sample_ids:
 #     df_nice = df_nice.append({'Sample #': i}, ignore_index=True)
 # print(df_nice)
--- processing/time_sync_to_csv.py
+++ processing/time_sync_to_csv.py
@@ -1,5 +1,7 @@
 import csv
 import datetime
+import time
+
 
 from utils import Interval
 import sys
@@ -28,16 +30,18 @@ def send_server_info(ip):
     message = "CONTROL_COMMAND:" + chr(10) + chr(32) + "position_server: " + ip + ":" + str(LISTEN_PORT)
     broadcastSocket.sendto(message.encode(), (BROADCAST_IP, BROADCAST_PORT))
 
+
 now = datetime.datetime.now()
 file_name = "raw_sync_data_" + now.strftime("%d-%m-%Y(%H_%M)") + ".csv"
 
 
-
 with open(file_name, 'w', newline='') as csvfile:
-    fieldnames = ['Event_ID', 'Node', 'Timestamp(in microsec)']
+    fieldnames = ["Local_time", 'Event_ID', 'Node', 'Timestamp']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     csvfile.close()
+
+
 def main(argv):
     opts, args = getopt.getopt(argv, "cghilt:o", ["ip="])
     del args
@@ -63,21 +67,31 @@ def main(argv):
 
     while True:
         try:
+
+            # Loads the incoming data into a json format
             raw_data, addr = listenSocket.recvfrom(1024)
             data = json.loads(raw_data)
             ip = addr[0]
+
+            # Gets the computer clock for the moment the data sample is recived
+            # and adds it to the raw .csv-file
+            local_time = datetime.datetime.now()
+            local_time = local_time.strftime("%H:%M:%S")
+
+
             event_id = data['timetic']
             timestamp = data['drift']
 
+
             with open(file_name, 'a', newline='') as csvfile:
-                fieldnames = ['Event_ID', 'Node', 'Timestamp(in microsec)']
+                fieldnames = ["Local_time", 'Event_ID', 'Node', 'Timestamp']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                thisdict = {"Event_ID": event_id, "Node": ip, "Timestamp(in microsec)": timestamp}
+                thisdict = {"Local_time": local_time, "Event_ID": event_id, "Node": ip, "Timestamp": timestamp}
                 print(thisdict)
                 writer.writerow(thisdict)
                 csvfile.close()
 
-
+            time.sleep(2)
 
         except KeyboardInterrupt:
             print("Shutting down interval...")
