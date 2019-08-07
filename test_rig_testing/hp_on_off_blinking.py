99505aa72c05f4158a9ab13e5ed958239fbf553f
Erik Robstad <erikvr@ntnu.no>
2019-08-07 07:38:40 +0200

    Swapped data[16] value for HP led On and OFF

--- test_rig_testing/hp_on_off_blinking.py
+++ test_rig_testing/hp_on_off_blinking.py
@@ -11,14 +11,12 @@ data = [0x43, 0x4f, 0x4e, 0x54, 0x52, 0x4f, 0x4c, 0x5f, 0x43,
         0x4f, 0x4d, 0x4d, 0x41, 0x4e, 0x44, 0x3a, 0x28, 0x00]
 
 while(True):
-    data[16] = 0x29
-    blinking_socket.sendto(bytearray(data), ('255.255.255.255', UDP_PORT))
-    print("ON")
-
-    time.sleep(1)
-
     data[16] = 0x28
     blinking_socket.sendto(bytearray(data), ('255.255.255.255', UDP_PORT))
-    print("OFF")
+    print("ON")
+    time.sleep(3)
 
-    time.sleep(1)
+    data[16] = 0x29
+    blinking_socket.sendto(bytearray(data), ('255.255.255.255', UDP_PORT))
+    print("OFF")
+    time.sleep(3)
